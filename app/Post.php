b01e2a8d0541f3a7e7e26fe72baa21bc3add7028
yassinOrlando <yassinovp@gmail.com>
2020-08-18 19:40:00 -0500

    Creation of models/providers and relations for ORM of author, category and post

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $table = 'posts';
+
+    public function post(){
+        return $this->belongsTo('App\Author');
+    }
+
+    public function post(){
+        return $this->belongsTo('App\Category');
+    }
 }
